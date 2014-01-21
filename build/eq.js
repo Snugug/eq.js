278b488dcb74aa130b89abb0b576cf42544d59b6
Sam Richard <snugug@gmail.com>
2014-01-21 13:38:27 -0500

    Fixed spacing issue

--- build/eq.js
+++ build/eq.js
@@ -74,9 +74,9 @@
     if (elem.addEventListener) {
       elem.addEventListener(event, fn, false);
     } else {
-      elem.attachEvent('on' + event, function() {
+      elem.attachEvent('on' + event, function () {
         // set the this pointer same as addEventListener when fn is called
-        return(fn.call(elem, window.event));
+        return (fn.call(elem, window.event));
       });
     }
   }
