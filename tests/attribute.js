7d90548982c33fc31a584531cf6678f30eacc2fe
Sam Richard <snugug@gmail.com>
2014-12-30 12:52:25 -0500

    Removed async from attribute test

--- tests/attribute.js
+++ tests/attribute.js
@@ -1,7 +1,6 @@
 describe('Set the `data-eq-state` attribute based on element width', function () {
   var elem = document.createElement('div'),
       body = document.querySelector('body'),
-      async = new AsyncSpec(this),
       sizes = [],
       result;
 
@@ -32,82 +31,75 @@ describe('Set the `data-eq-state` attribute based on element width', function ()
   //////////////////////////////
   // No Sizes
   //////////////////////////////
-  async.it('should have no state if its width is smaller than smallest size', function (done) {
+  it('should have no state if its width is smaller than smallest size', function () {
     body.style.width = (sizes[0] - 1) + 'px';
     eqjs.refreshNodes();
     eqjs.query(undefined, function () {
       result = elem.getAttribute('data-eq-state');
       expect(result).toBe(null);
-      done();
     });
   });
 
   //////////////////////////////
   // Small Sizes
   //////////////////////////////
-  async.it('should be `small` when its width is equal to its smallest size', function (done) {
+  it('should be `small` when its width is equal to its smallest size', function () {
     body.style.width = (sizes[0]) + 'px';
     eqjs.refreshNodes();
     eqjs.query(undefined, function () {
       result = elem.getAttribute('data-eq-state');
       expect(result).toBe('small');
-      done();
     });
   });
 
-  async.it('should be `small` when its width is larger to its smallest size but smaller than its `medium` size', function (done) {
+  it('should be `small` when its width is larger to its smallest size but smaller than its `medium` size', function () {
     body.style.width = (sizes[0] + 1) + 'px';
     eqjs.refreshNodes();
     eqjs.query(undefined, function () {
       result = elem.getAttribute('data-eq-state');
       expect(result).toBe('small');
-      done();
     });
   });
 
   //////////////////////////////
   // Medium Sizes
   //////////////////////////////
-  async.it('should be `medium` when its width is equal to its smallest size', function (done) {
+  it('should be `medium` when its width is equal to its smallest size', function () {
     body.style.width = (sizes[1]) + 'px';
     eqjs.refreshNodes();
     eqjs.query(undefined, function () {
       result = elem.getAttribute('data-eq-state');
       expect(result).toBe('medium');
-      done();
     });
   });
 
-  async.it('should be `medium` when its width is larger to its medium size but smaller than its `larger` size', function (done) {
+  it('should be `medium` when its width is larger to its medium size but smaller than its `larger` size', function () {
     body.style.width = (sizes[1] + 1) + 'px';
     eqjs.refreshNodes();
     eqjs.query(undefined, function () {
       result = elem.getAttribute('data-eq-state');
       expect(result).toBe('medium');
-      done();
     });
   });
 
   //////////////////////////////
   // Large Sizes
   //////////////////////////////
-  async.it('should be `large` when its width is equal to its largest size', function (done) {
+  it('should be `large` when its width is equal to its largest size', function () {
     body.style.width = (sizes[2]) + 'px';
     eqjs.refreshNodes();
     eqjs.query(undefined, function () {
       result = elem.getAttribute('data-eq-state');
       expect(result).toBe('large');
-      done();
     });
   });
 
-  async.it('should be `large` when its width is larger to its largest size', function (done) {
+  it('should be `large` when its width is larger to its largest size', function () {
     body.style.width = (sizes[2] + 1) + 'px';
     eqjs.refreshNodes();
     eqjs.query(undefined, function () {
       result = elem.getAttribute('data-eq-state');
       expect(result).toBe('large');
-      done();
     });
   });
 });
